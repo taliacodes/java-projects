e28bf56fb55b2b0df028369dc681260241c6e4e8
Talia Fayaz <taliafayazc@gmail.com>
2019-10-03 18:29:11 -0400

    create toString() method

--- testing.java
+++ testing.java
@@ -1,9 +1,22 @@
 import java.util.Arrays;
 
-class Quickstart {
-  public static void main(String[] args) {
-    String[] menuItems = {"pizza", "soup", "lasagna"};
-    String menuItemsPrintout = Arrays.toString(menuItems);
-    System.out.println(menuItemsPrintout);
+class Dog {
+  String breed;
+  int age;
+
+  public Dog(String dogBreed, int dogYears) {
+    breed = dogBreed;
+    age = dogYears;
   }
+
+  public static void main(String[] args) {
+    System.out.println("Is this working?");
+    String[] items = {"these", "are", "the", "items", "apple"};
+    Dog poochie = new Dog("Doberman", 75);
+    System.out.println(poochie.toString());
+  }
+
+  public String toString(){
+    return "This is a " + breed + " dog!";
+}
 }
